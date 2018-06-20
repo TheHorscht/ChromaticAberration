a005a05cb4416968f94bad98398e96e9063deb6f
The Horscht <realhorscht@gmail.com>
2018-06-20 16:19:37 +0200

    turbuleeent

--- script.js
+++ script.js
@@ -5,6 +5,31 @@ async function applyChromaticAberration() {
 	svgContainer.innerHTML = svgFileContent;
 	document.body.appendChild(svgContainer);
 	document.body.classList.add("chromatic-aberration");
+
+	let beep = 0;
+	let svg = document.querySelector("svg");
+	let turbulence = document.querySelector("feTurbulence");
+	let dir = 1;
+	function loop() {
+		turbulence.setAttribute("baseFrequency", 0.005 + Math.cos(beep * Math.PI * 2) * 0.0005);
+		beep += 0.005 * dir;
+		if(beep > 1 || beep < 0) {
+			dir *= -1;
+		}
+		svg.setAttribute("width", "0");
+		window.requestAnimationFrame(loop);
+	}
+	window.requestAnimationFrame(loop);
 }
 
 applyChromaticAberration();
+
+/*
+
+How to do chromatic aberration with displacement:
+Use an image as the displacement map source feImage
+Offset that image to move it around
+feImage displace.jpg
+feOffset dx dy = mousepos, in=feImgae result=disOffset
+displacementMap input=disOffset
+ */
