f8bff86b6980fc84cfc8ddf73e8cf8c21a44d470
The Horscht <realhorscht@gmail.com>
2018-06-20 16:55:31 +0200

    wavy

--- script.js
+++ script.js
@@ -6,15 +6,31 @@ async function applyChromaticAberration() {
 	document.body.appendChild(svgContainer);
 	document.body.classList.add("chromatic-aberration");
 
-	let beep = 0;
+	let beepR = 0;
+	let beepG = 0;
+	let beepB = 0;
 	let svg = document.querySelector("svg");
-	let turbulence = document.querySelector("feTurbulence");
-	let dir = 1;
+	let turbuR = document.querySelector("#turbuR");
+	let turbuG = document.querySelector("#turbuG");
+	let turbuB = document.querySelector("#turbuB");
+	let dirR = 1;
+	let dirG = 1;
+	let dirB = 1;
 	function loop() {
-		turbulence.setAttribute("baseFrequency", 0.005 + Math.cos(beep * Math.PI * 2) * 0.0005);
-		beep += 0.005 * dir;
-		if(beep > 1 || beep < 0) {
-			dir *= -1;
+		turbuR.setAttribute("baseFrequency", 0.005 + Math.cos(beepR * Math.PI * 2) * 0.005);
+		turbuG.setAttribute("baseFrequency", 0.005 + Math.cos(beepG * Math.PI * 2) * 0.005);
+		turbuB.setAttribute("baseFrequency", 0.005 + Math.cos(beepB * Math.PI * 2) * 0.005);
+		beepR += 0.0005 * dirR;
+		beepG += 0.0008 * dirG;
+		beepB += 0.00037 * dirB;
+		if(beepR > 1 || beepR < 0) {
+			dirR *= -1;
+		}
+		if(beepG > 1 || beepG < 0) {
+			dirG *= -1;
+		}
+		if(beepB > 1 || beepB < 0) {
+			dirB *= -1;
 		}
 		svg.setAttribute("width", "0");
 		window.requestAnimationFrame(loop);
