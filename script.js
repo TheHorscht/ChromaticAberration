98e8b4c46bf1de128fa412a6bc987686cff1e860
The Horscht <realhorscht@gmail.com>
2018-06-21 15:29:43 +0200

    refactored

--- script.js
+++ script.js
@@ -18,43 +18,57 @@ async function applyChromaticAberration() {
 		document.body.style += ";filter:url(#chromatic-aberration);";
 	}
 
-	let beepR = 0;
-	let beepG = 0;
-	let beepB = 0;
-	let svg = document.querySelector("svg");
+	let progress = {
+		r: createPingPongValue(),
+		g: createPingPongValue(),
+		b: createPingPongValue()
+	}
+	let svg = document.getElementById("chromatic-aberration-effects");
 	let turbuR = document.querySelector("#turbuR");
 	let turbuG = document.querySelector("#turbuG");
 	let turbuB = document.querySelector("#turbuB");
-	let dirR = 1;
-	let dirG = 1;
-	let dirB = 1;
+	function createPingPongValue() {
+		let value = 0;
+		let dir = 1;
+		return {
+			value() {
+				return value;
+			},
+			advance(t) {
+				value += t * dir;
+				if(value > 1 || value < 0) {
+					dir *= -1;
+				}
+			}
+		};
+	}
 	function loop() {
-		turbuR.setAttribute("baseFrequency", 0.005 + Math.cos(beepR * Math.PI * 2) * 0.005);
-		turbuG.setAttribute("baseFrequency", 0.005 + Math.cos(beepG * Math.PI * 2) * 0.005);
-		turbuB.setAttribute("baseFrequency", 0.005 + Math.cos(beepB * Math.PI * 2) * 0.005);
-		beepR += 0.0000152 * settings.waveSpeed * dirR;
-		beepG += 0.0000223 * settings.waveSpeed * dirG;
-		beepB += 0.0000117 * settings.waveSpeed * dirB;
-		if(beepR > 1 || beepR < 0) {
-			dirR *= -1;
-		}
-		if(beepG > 1 || beepG < 0) {
-			dirG *= -1;
-		}
-		if(beepB > 1 || beepB < 0) {
-			dirB *= -1;
-		}
+		turbuR.setAttribute("baseFrequency", 0.005 + Math.cos(progress.r.value() * Math.PI * 2) * 0.005);
+		turbuG.setAttribute("baseFrequency", 0.005 + Math.cos(progress.g.value() * Math.PI * 2) * 0.005);
+		turbuB.setAttribute("baseFrequency", 0.005 + Math.cos(progress.b.value() * Math.PI * 2) * 0.005);
+		progress.r.advance(0.0000152 * settings.waveSpeed);
+		progress.g.advance(0.0000223 * settings.waveSpeed);
+		progress.b.advance(0.0000117 * settings.waveSpeed);
 		svg.setAttribute("width", "0");
 		window.requestAnimationFrame(loop);
+	}
+
+	function setEffectStrength(svg, x, y) {
+		console.log(svg);
+		
+		let offsetRed = svg.getElementById("chromatic-aberration-offset-red");
+		let offsetBlue = svg.getElementById("chromatic-aberration-offset-blue");
+		offsetRed.setAttribute("dx", x);
+		offsetRed.setAttribute("dy", y);
+		offsetBlue.setAttribute("dx", -x);
+		offsetBlue.setAttribute("dy", -y);
+		svg.setAttribute("width", "0");
 	}
 
 	chrome.runtime.onMessage.addListener(msg => {
 		if(msg.command === "settingsChanged") {
 			if(msg.setting === "strength") {
-				let feOffsets = document.querySelectorAll("#chromatic-aberration feOffset");
-				feOffsets[0].setAttribute("dx", -msg.value);
-				feOffsets[2].setAttribute("dx", msg.value);
-				svgContainer.querySelector("svg").setAttribute("width", "0");
+				setEffectStrength(svg, msg.value, 0);
 			} else if(msg.setting === "waveSpeed") {
 				settings.waveSpeed = msg.value;
 			} else if(msg.setting === "enabled") {
@@ -62,13 +76,9 @@ async function applyChromaticAberration() {
 				let svg = svgContainer.querySelector("svg");
 				console.log(msg, feOffsets, svg);
 				if(msg.value === true) {
-					feOffsets[0].setAttribute("dx", -setting.strength);
-					feOffsets[2].setAttribute("dx", setting.strength);
-					svg.setAttribute("width", "0");
+					setEffectStrength(svg, settings.strength, 0);
 				} else {
-					feOffsets[0].setAttribute("dx", 0);
-					feOffsets[2].setAttribute("dx", 0);
-					svg.setAttribute("width", "0");
+					setEffectStrength(svg, 0, 0);
 				}
 			} else if(msg.setting === "wavy") {
 				settings.waveSpeed = msg.value;
